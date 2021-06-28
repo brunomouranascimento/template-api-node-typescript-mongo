7ddb860ad6677f98b4aace10dc5be46c569542d2
Bruno Nascimento <bruno.bmn@gmail.com>
2021-06-28 16:21:52 -0300

    feat: adjusting return type

--- src/app/controllers/v1/AuthController.ts
+++ src/app/controllers/v1/AuthController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from 'express';
 import AuthService from '@services/AuthService';
 import ApiResponse from '@core/ApiResponse';
 
-import { IResponse } from '@interfaces';
+import { IResponse, IUser } from '@interfaces';
 
 class AuthController {
-  async authenticate(req: Request, res: Response): Promise<IResponse<void>> {
+  async authenticate(req: Request, res: Response): Promise<IResponse<IUser>> {
     try {
       const data = await AuthService.authenticate(req.body);
       return ApiResponse.send(200, req, res, data);
