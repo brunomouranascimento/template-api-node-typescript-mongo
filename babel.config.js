1eda8149de1b74f4f4198706155506e81c967277
Bruno Nascimento <bruno.bmn@gmail.com>
2021-07-23 13:46:35 -0300

    feat: fix to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,31 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript'
+    '@babel/preset-typescript',
   ],
   plugins: [
-    ['module-resolver', {
-      alias: {
-        '@modules': './src/modules',
-        '@config': './src/config',
-        '@shared': './src/shared'
-      }
-    }],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@controllers': './src/app/controllers',
+          '@models': './src/app/models',
+          '@config': './src/config',
+          '@database': './src/database',
+          '@routes': './src/app/routes/routes',
+          '@repositories': './src/app/repositories',
+          '@services': './src/app/services',
+          '@interfaces': './src/app/interfaces/interfaces',
+          '@dtos': './src/app/dtos/dtos',
+          '@middlewares': './src/app/middlewares',
+          '@utils': './src/app/utils',
+          '@documentation': './src/app/documentation',
+          '@core': './src/app/core',
+        },
+      },
+    ],
     'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
-}
+};
